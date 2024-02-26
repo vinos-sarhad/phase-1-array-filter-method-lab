1917589c760f774f3333648307bdf55a661df6b8
Vinos Sarhad <vinos.sarhadd@gmail.com>
2024-02-26 15:57:39 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+const drivers = ["John", "Alice","ali","Bob", "Mike"];
+
+ function findMatching(drivers,string){
+    const lowerCaseName = string.toLowerCase();
+    return  drivers.filter(driver=> driver.toLowerCase()===lowerCaseName)
+         
+ }
+
+
+
+ const matchingDrivers =findMatching(drivers,"John")
+ console.log(matchingDrivers )
+
+
+
+
+
+function fuzzyMatch (drivers, query){
+
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+
+
+
+}
+// console.log(fuzzyMatch(drivers,"a"))
+function matchName (drivers, string){
+return drivers.filter(driver=> driver.name===string)
+}
+
+
+
+ 
